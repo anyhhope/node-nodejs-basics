961fdfabd8e37c8a066d063ca0f2eec7fb383a24
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 11:45:17 +0300

    fs list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { opendir } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = join(__dirname, 'files/');
+    try {
+        const dir = await opendir(dirPath)
+        for await (const dirent of dir) {
+            console.log(dirent.name);
+        }
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed');
+        } else {
+            console.error(error);
+        }
+    }
 };
 
 await list();
