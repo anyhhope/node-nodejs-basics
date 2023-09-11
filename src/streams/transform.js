3cc12f063612195ee269fb0a6b65862eee67d636
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 14:40:30 +0300

    streams transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
+const reverseTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        this.push(chunk.toString().split('').reverse().join(''));
+        callback();
+    }
+});
+
 const transform = async () => {
-    // Write your code here 
+    stdin.pipe(reverseTransform).pipe(stdout);
 };
 
 await transform();
