e9b90694eecfe88330185773481e3689b3a23cd2
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 11:42:55 +0300

    fs delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { rm } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    rm(join(__dirname, 'files/filetoRemove.txt'))
+        .catch((error) => {
+            if(error.code == 'ENOENT') console.error('FS operation failed');
+            else console.error(error);
+        });
 };
 
 await remove();
