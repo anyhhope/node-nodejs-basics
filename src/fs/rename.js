cfa5884da2dc7b6072597b2baedf241d52955359
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 11:36:09 +0300

    fs rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { rename as renameFunc } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    renameFunc(join(__dirname, 'files/wrongFilename.txt'), join(__dirname, 'files/properFilename.md'))
+        .catch((error) => {
+            if(error.code == 'ENOENT') console.error('FS operation failed');
+            else console.error(error);
+        });
 };
 
 await rename();
