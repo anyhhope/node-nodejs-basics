b44c00684f14ebacfd33a383b12b712ad9a2f554
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 10:47:12 +0300

    fs - createfile

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { writeFile } from 'fs/promises';
+import { join } from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const filepath = join(__dirname, 'files/fresh.txt');
+    writeFile(filepath, 'I am fresh and young', { encoding: 'utf8', flag: 'wx' })
+        .catch(error => {
+            if (error.code === 'EEXIST') {
+                console.error('FS operation failed');
+            } else {
+                console.error(error);
+            }
+        });
 };
 
 await create();
