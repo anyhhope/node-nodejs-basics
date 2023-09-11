8c990a657eac671a81d27cbe534415e8a785b5c4
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 11:50:54 +0300

    fs read.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const remove = async () => {
-    rm(join(__dirname, 'files/filetoRemove.txt'))
+    rm(join(__dirname, 'files/fileToRemove.txt'))
         .catch((error) => {
             if(error.code == 'ENOENT') console.error('FS operation failed');
             else console.error(error);
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const contents = await readFile(join(__dirname, 'files/fileToRead.txt'), { encoding: 'utf8' });
+        console.log(contents);
+    } catch (error) {
+        if(error.code == 'ENOENT') console.log('FS operation failed');
+        else console.error(error);;
+    }
 };
 
 await read();
