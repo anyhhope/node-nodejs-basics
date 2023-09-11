132529954153a14de67ebbc053ad1e3b5d64e15b
anyhhope <hopeanysimova@yandex.ru>
2023-09-11 13:51:23 +0300

    streams read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { open } from 'fs/promises';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const file = await open(join(__dirname, 'files/fileToRead.txt'))
+        .catch((error) => {
+            if(error.code == 'ENOENT') console.error('FS stream aoperation failed');
+            else console.error(error);
+        })
+    file.createReadStream().pipe(stdout);
 };
 
 await read();
